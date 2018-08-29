e23fe1905b53e7b1626337ff208f0231f43ffd84
Minh Duong <minhduong.md25@gmail.com>
2018-08-30 02:12:03 +0700

    Add files via upload
    add files

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+    
+    $link = mysqli_connect("127.0.0.1", "root", "Reyna5111!", "mydb");
+    
+    if (mysqli_connect_error()) {
+        
+        die ("Database Connection Error");
+        
+    }
+
+?>
